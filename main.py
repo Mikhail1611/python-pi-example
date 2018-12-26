527ee174245d24408eb56e1790a870c98f88d892
Mikhail <m.krecker@web.de>
2018-12-26 15:42:32 +0100

    merged conflicts resolved

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def main (msg):
-<<<<<<< HEAD
 // Adding the second comment to this file.
-=======
   //Print a message
->>>>>>> refs/remotes/origin/master
     print (msg)
 main("Hello People")
